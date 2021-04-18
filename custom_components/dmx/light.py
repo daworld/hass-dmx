e0b32bb0ca1585528d2c817fac6087469767cd79
daworld <edwin@daworld.nl>
2021-04-18 23:14:35 +0200

    Update light.py
    Send packet twice for sure

--- custom_components/dmx/light.py
+++ custom_components/dmx/light.py
@@ -492,6 +492,7 @@ class DMXGateway(object):
         packet = self._base_packet[:]
         packet.extend(self._channels)
         self._socket.sendto(packet, (self._host, self._port))
+        self._socket.sendto(packet, (self._host, self._port)) # Send twice
         _LOGGER.debug("Sending Art-Net frame")
 
     def set_channels(self, channels, value, send_immediately=True):
